e3115e9a44b79c8204a30224ec1760012d91156e
Lennart Breede <lennart.breede@gmail.com>
2026-06-12 09:12:08 -0700

    Updated problem 1

--- c/problem-1/main.c
+++ c/problem-1/main.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
-int main()
-{
-    int sum = 0;
-
-    for (int i = 1; i < 1000; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-            sum += i;
+int main() {
+  int sum = 0;
+  for (int i = 0; i < 1000; ++i) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
     }
-
-    printf("The sum is: %d\n", sum);
-
-    return 0;
+  }
+  printf("The sum is: %d\n", sum);
+  return 0;
 }
